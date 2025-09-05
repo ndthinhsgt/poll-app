7b451180db346420650d01a85e6af90e84f6fa3c
nvxquynhsgt <nvxquynh.sgt@gmail.com>
2025-09-05 08:52:36 +0700

    #14 bring goback function to common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -80,3 +80,11 @@ function getUserFromSession() {
   return { id: userId, name: userName };
 };
 //end
+
+function goBack(){
+    if (window.history.length > 1){
+        window.history.back();
+    } else {
+        alert("Không có trang trước!");
+    }
+}
--- assets/js/create.js
+++ assets/js/create.js
@@ -4,11 +4,3 @@ window.addEventListener("DOMContentLoaded", () => {
 
     document.querySelector('#back').addEventListener("click", goBack);
 });
-
-function goBack(){
-    if (window.history.length > 1){
-        window.history.back();
-    } else {
-        alert("Không có trang trước!");
-    }
-}
