db7f16a48f1b302e939bb0cf9f0351c4d6dafb64
nvxquynhsgt <nvxquynh.sgt@gmail.com>
2025-09-05 08:55:47 +0700

    #15 bring logout function to common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -80,3 +80,14 @@ function getUserFromSession() {
   return { id: userId, name: userName };
 };
 //end
+
+function logOut() {
+  const logSession = JSON.parse(sessionStorage.getItem("loginUser"));
+  if (!logSession) {
+    window.location.href = "/login";
+  } else {
+    alert("Bạn có muốn đăng xuất?");
+    sessionStorage.removeItem("loginUser");
+    window.location.href = "/login";
+  }
+}
